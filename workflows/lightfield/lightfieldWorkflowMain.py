81ac33cb2b643e37793d419e0d3a0ac1a8d6be3f
fvclaus <=>
2012-10-11 15:18:20 +0200

    added start script for lightfield workflow

---
+++ workflows/lightfield/lightfieldWorkflowMain.py
@@ -0,0 +1,12 @@
+'''
+Created on Oct 11, 2012
+
+@author: fredo
+'''
+
+from ilastik.shell.gui.startShellGui import startShellGui
+from lightfieldWorkflow import LightfieldWorkflow
+
+
+if __name__ == "__main__":
+    startShellGui(LightfieldWorkflow)
