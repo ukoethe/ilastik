deef8ec151674a323ba67c6f5c42a452b2fa82f4
Stuart Berg <bergs@janelia.hhmi.org>
2012-10-31 16:29:57 -0400

    updated update script

--- update_from_master.sh
+++ update_from_master.sh
@@ -15,4 +15,27 @@ rm -rf html
 git add .
 git commit -m "Added new docs"
 cp ../$0 .
-./prepare_for_github.sh
+
+# Prepare the files for github...
+for fl in `find . -name "*.html"` `find . -name "*.txt"` `find . -name "*.svg"` `find . -name "*.js"` `find . -name "*.css"`
+do
+  echo "Processing $fl"
+  mv $fl $fl.old
+  sed 's/_images/images/g' $fl.old > $fl
+  mv $fl $fl.old
+  sed 's/_modules/modules/g' $fl.old > $fl
+  mv $fl $fl.old
+  sed 's/_static/static/g' $fl.old > $fl
+  mv $fl $fl.old
+  sed 's/_sources/sources/g' $fl.old > $fl
+  rm -f $fl.old
+done
+git mv _images images
+git mv _modules modules
+git mv _sources sources
+git mv _static static
+
+git add -u .
+
+git commit -m "Prepared html files for output on github pages, which doesn't allow directories that begin with underscores."
+
