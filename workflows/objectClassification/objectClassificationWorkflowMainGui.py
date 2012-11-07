a990138b8d5b1c4f9d51b64e7b63f1c2f1c4b3a7
Kemal Eren <kemal.eren@iwr.uni-heidelberg.de>
2012-11-07 10:59:33 +0100

    do not debug by default

--- workflows/objectClassification/objectClassificationWorkflowMainGui.py
+++ workflows/objectClassification/objectClassificationWorkflowMainGui.py
@@ -6,15 +6,12 @@ def debug_with_existing(shell, workflow):
     """
     (Function for debug and testing.)
     """
-    projFilePath = "/home/akreshuk/bla.ilp"
-    #projFilePath = "/home/mschiegg/bla.ilp"
-    #projFilePath = '/magnetic/gigacube.ilp'
-    #projFilePath = '/home/bergs/Downloads/synapse_detection_training1.ilp'
-    #projFilePath = '/magnetic/250-2.ilp'
+    # set this variable
+    projFilePath = ""
     # Open a project
     shell.openProjectFile(projFilePath)
 
 
 if __name__=="__main__":
-    startShellGui( ObjectClassificationWorkflow, debug_with_existing )
-    #startShellGui(ObjectClassificationWorkflow)
+    #startShellGui( ObjectClassificationWorkflow, debug_with_existing )
+    startShellGui(ObjectClassificationWorkflow)
